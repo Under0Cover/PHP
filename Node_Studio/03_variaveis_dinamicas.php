890352eb1fc3f2da8ea814c0b337317f250748ab
Under0Cover <60072961+Under0Cover@users.noreply.github.com>
2021-01-04 15:09:49 -0300

    Aula_03 - Variáveis Dinâmicas

---
+++ Node_Studio/03_variaveis_dinamicas.php
@@ -0,0 +1,15 @@
+<?php
+// Variáveis Dinâmicas
+$bebida = "Refrigerante";
+$$bebida = "Guaraná";
+
+echo $Refrigerante;
+echo "<br>";
+
+$destino = "Cidade";
+$$destino = "Ilhéus";
+
+echo $destino;
+echo "<br>";
+echo $Cidade;
+?>
