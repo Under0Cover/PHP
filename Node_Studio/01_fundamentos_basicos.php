d95fb1e08341dc409a5f890f8594f261945d97a4
Under0Cover <60072961+Under0Cover@users.noreply.github.com>
2021-01-04 13:21:14 -0300

    Aula_01 - Fundamentos básicos

---
+++ Node_Studio/01_fundamentos_basicos.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fundamentos Básicos</title>
+</head>
+<body>
+    <?php
+        echo "Meu nome é Tales Oliver <br>";
+        // Comentário em PHP
+        print "Minha idade é 36 anos";
+    ?>
+</body>
+</html>
