2fd317fcf52c7ffcf8d3639b83fa43601c684dd1
Under0Cover <60072961+Under0Cover@users.noreply.github.com>
2021-01-04 14:59:41 -0300

    Aula_02 - Variáveis

---
+++ Node_Studio/02_variaves.php
@@ -0,0 +1,36 @@
+<?php
+// Meus dados
+$nome = "Tales Oliver";
+$idade = 36;
+$altura = 1.76;
+
+echo "Meu nome é $nome, minha idade é $idade e minha altura é $altura m <br>";
+
+// Alterando meus dados
+$nome = "Felipe Cabral";
+$idade = 29;
+$altura = 1.90;
+echo "Meu nome é $nome, minha idade é $idade e minha altura é $altura m <br>";
+echo "<hr>";
+
+$carro = "Hilux";
+$Carro = "Fusca";
+
+echo $carro;
+echo "<br> $Carro";
+
+// Nomes válidos
+//$nome
+//$_meunome
+//$carro10
+//$nota1
+//$meu_nome
+//$meuNome
+
+// Nomes inválidos
+//$123456
+//$carro*@
+//$meu carro
+
+
+?>
