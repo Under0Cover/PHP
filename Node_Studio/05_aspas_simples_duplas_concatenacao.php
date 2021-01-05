e5866b2f8ebe296975aba6699efe37e1058f5541
Under0Cover <60072961+Under0Cover@users.noreply.github.com>
2021-01-05 06:39:33 -0300

    Aula_05 - Aspas Simples\Duplas + Concatenação

---
+++ Node_Studio/05_aspas_simples_duplas_concatenacao.php
@@ -0,0 +1,17 @@
+<?php
+$nome = 'Tales Oliver';
+echo $nome;
+echo '<br>';
+echo 'Meu nome é $nome.';
+echo '<br>';
+echo 'Meu nome é '.$nome;
+echo '<br>';
+echo 'Meu nome é '.$nome. ' e minha idade é 36 anos.';
+echo '<br>';
+echo 'Meu nome é '.$nome. ' e minha idade é \'36\' anos.';
+echo '<br>';;
+echo "Meu nome é $nome.";
+echo '<br>';
+echo "Meu \"nome\" é $nome.";
+
+?>
