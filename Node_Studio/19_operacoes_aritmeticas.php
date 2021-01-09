b84b36e83a788c9bdef049d77522eadb806d9d69
Under0Cover <60072961+Under0Cover@users.noreply.github.com>
2021-01-09 13:12:24 -0300

    Aula_19 - Operações Aritmeticas

---
+++ Node_Studio/19_operacoes_aritmeticas.php
@@ -0,0 +1,71 @@
+<?php
+/* OPERADORES ARITMETICOS
+
+* ADIÇÃO
+* SUBTRAÇÃO
+* MULTIPLICAÇÃO
+* DIVISÃO
+* MÓDULO
+* EXPONENCIAÇÃO
+
+*/
+
+// Adição
+echo 10 + 10;
+echo '<hr>';
+
+// Subrtração
+echo 20 - 5;
+echo '<hr>';
+
+// Multiplicação
+echo 3 * 5;
+echo '<hr>';
+
+// Divisão
+echo 15 / 5;
+echo '<hr>';
+
+// Módulo
+echo 15 % 5;
+echo '<br>';
+
+// Exponenciação
+echo 3 ** 5;
+echo '<hr>';
+
+// Trabalhando com variáveis
+
+$valor_01 = 10;
+$valor_02 = 20;
+$valor_03 = 30;
+$valor_04 = 5;
+$valor_05 = 16;
+
+$adicao = $valor_01 + $valor_02;
+$subtracao = $valor_03 - $valor_01;
+$multiplicacao = $valor_04 - $valor_01;
+$divisao = $valor_03 / $valor_01;
+$modulo = $valor_05 % $valor_04;
+$expone = $valor_01 ** $valor_04;
+
+echo $adicao;
+echo '<hr>';
+echo $subtracao;
+echo '<hr>';
+echo $multiplicacao;
+echo '<hr>';
+echo $divisao;
+echo '<hr>';
+echo $modulo;
+echo '<hr>';
+echo $expone;
+echo '<hr>';
+
+// Média
+echo (5 + 7 + 9 + 8) / 4;
+
+
+
+
+?>
