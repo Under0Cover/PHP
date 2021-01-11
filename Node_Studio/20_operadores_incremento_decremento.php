10276761e20d413ae0dd0cf01317873943ca1fe7
Under0Cover <60072961+Under0Cover@users.noreply.github.com>
2021-01-11 08:47:30 -0300

    Aula_20 - Operadores incremento e decremento

---
+++ Node_Studio/20_operadores_incremento_decremento.php
@@ -0,0 +1,33 @@
+<?php
+
+/* OPERADORES DE INCREMENTO E DECREMENTO
+
+* SERVEM PARA SOMAR OU SUBTRAIR EM (+1 OU -1)
+* QUALQUER VALOR DO TIPO NÚMERICO OU STRING
+
+*/
+
+$valorInc = 20;
+$valorDec = 20;
+
+// Incremento:
+// Pré-incremento
+echo ++$valorInc;
+echo '<hr>';
+
+// Pós-incremento
+echo $valorInc++;
+echo $valorInc;
+echo '<hr>';
+
+// Decremento:
+// Pré-decremento
+echo --$valorDec;
+echo '<hr>';
+
+// Pós-decremento
+echo $valorDec--;
+echo $valorDec;
+echo '<hr>';
+
+?>
