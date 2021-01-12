986472918ac67bd4914d3663befdc4b405662f03
Under0Cover <60072961+Under0Cover@users.noreply.github.com>
2021-01-11 22:45:05 -0300

    Aula_22 - Operadores de comparação

---
+++ Node_Studio/22_operadores_comparacao.php
@@ -0,0 +1,226 @@
+<?php
+
+/* OPERADORES DE COMPARAÇÃO */
+
+$variavel_a = 10;
+$variavel_b = 10;
+$variavel_c = '10';
+$variavel_d = 5;
+
+if ($variavel_a == $variavel_b):
+    echo 'Positivo';
+else:
+    echo 'Negativo';
+endif;
+
+echo '<hr>';
+
+if ($variavel_b == $variavel_c):
+    echo 'Positivo';
+else:
+    echo 'Negativo';
+endif;
+
+echo '<hr>';
+
+if ($variavel_c != $variavel_d):
+    echo 'Positivo';
+else:
+    echo 'Negativo';
+endif;
+
+echo '<hr>';
+
+if ($variavel_a === $variavel_b):
+    echo 'Positivo';
+else:
+    echo 'Negativo';
+endif;
+
+echo '<hr>';
+
+if ($variavel_b === $variavel_c):
+    echo 'Positivo';
+else:
+    echo 'Negativo';
+endif;
+
+echo '<hr>';
+
+if ($variavel_c === $variavel_d):
+    echo 'Positivo';
+else:
+    echo 'Negativo';
+endif;
+
+echo '<hr>';
+
+if ($variavel_a !== $variavel_b):
+    echo 'Positivo';
+else:
+    echo 'Negativo';
+endif;
+
+echo '<hr>';
+
+if ($variavel_b !== $variavel_c):
+    echo 'Positivo';
+else:
+    echo 'Negativo';
+endif;
+
+echo '<hr>';
+
+if ($variavel_c !== $variavel_d):
+    echo 'Positivo';
+else:
+    echo 'Negativo';
+endif;
+
+echo '<hr>';
+
+if ($variavel_a <> $variavel_b):
+    echo 'Positivo';
+else:
+    echo 'Negativo';
+endif;
+
+echo '<hr>';
+
+if ($variavel_b <> $variavel_c):
+    echo 'Positivo';
+else:
+    echo 'Negativo';
+endif;
+
+echo '<hr>';
+
+if ($variavel_c <> $variavel_d):
+    echo 'Positivo';
+else:
+    echo 'Negativo';
+endif;
+
+echo '<hr>';
+
+if ($variavel_a < $variavel_b):
+    echo 'Positivo';
+else:
+    echo 'Negativo';
+endif;
+
+echo '<hr>';
+
+if ($variavel_b < $variavel_c):
+    echo 'Positivo';
+else:
+    echo 'Negativo';
+endif;
+
+echo '<hr>';
+
+if ($variavel_c < $variavel_d):
+    echo 'Positivo';
+else:
+    echo 'Negativo';
+endif;
+
+echo '<hr>';
+
+if ($variavel_a > $variavel_b):
+    echo 'Positivo';
+else:
+    echo 'Negativo';
+endif;
+
+echo '<hr>';
+
+if ($variavel_b > $variavel_c):
+    echo 'Positivo';
+else:
+    echo 'Negativo';
+endif;
+
+echo '<hr>';
+
+if ($variavel_c > $variavel_d):
+    echo 'Positivo';
+else:
+    echo 'Negativo';
+endif;
+
+echo '<hr>';
+
+if ($variavel_a <= $variavel_b):
+    echo 'Positivo';
+else:
+    echo 'Negativo';
+endif;
+
+echo '<hr>';
+
+if ($variavel_b <= $variavel_c):
+    echo 'Positivo';
+else:
+    echo 'Negativo';
+endif;
+
+echo '<hr>';
+
+if ($variavel_c <= $variavel_d):
+    echo 'Positivo';
+else:
+    echo 'Negativo';
+endif;
+
+echo '<hr>';
+
+if ($variavel_a >= $variavel_b):
+    echo 'Positivo';
+else:
+    echo 'Negativo';
+endif;
+
+echo '<hr>';
+
+if ($variavel_b >= $variavel_c):
+    echo 'Positivo';
+else:
+    echo 'Negativo';
+endif;
+
+echo '<hr>';
+
+if ($variavel_c >= $variavel_d):
+    echo 'Positivo';
+else:
+    echo 'Negativo';
+endif;
+
+echo '<hr>';
+
+if ($variavel_a <=> $variavel_b):
+    echo 'Positivo';
+else:
+    echo 'Negativo';
+endif;
+
+echo '<hr>';
+
+if ($variavel_b <=> $variavel_c):
+    echo 'Positivo';
+else:
+    echo 'Negativo';
+endif;
+
+echo '<hr>';
+
+if ($variavel_c <=> $variavel_d):
+    echo 'Positivo';
+else:
+    echo 'Negativo';
+endif;
+
+
+
+?>
