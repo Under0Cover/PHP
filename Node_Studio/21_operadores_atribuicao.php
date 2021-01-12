858c79780f9fd07b5d842f31e29baab029a6d153
Under0Cover <60072961+Under0Cover@users.noreply.github.com>
2021-01-11 22:20:28 -0300

    Aula_21 - Operadores Atribuição

---
+++ Node_Studio/21_operadores_atribuicao.php
@@ -0,0 +1,27 @@
+<?php
+
+/* OPERADORES DE ATRIBUIÇÃO */
+
+$variavel_a = 10;
+echo $variavel_a;
+echo '<br>';
+$variavel_b = 4;
+echo $variavel_b;
+echo '<br>';
+$variavel_a += $variavel_b;
+echo $variavel_a;
+echo '<br>';
+$variavel_a -= $variavel_b;
+echo $variavel_a;
+echo '<br>';
+$variavel_a *= $variavel_b;
+echo $variavel_a;
+echo '<br>';
+$variavel_a /= $variavel_b;
+echo '<br>';
+$variavel_a %= $variavel_b;
+echo $variavel_a;
+echo '<br>';
+
+
+?>
