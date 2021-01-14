abb5437547985d3c92a51e1de21031c49e9d7414
Under0Cover <60072961+Under0Cover@users.noreply.github.com>
2021-01-14 13:31:14 -0300

    Aula_31 - Superglobais (02)

---
+++ Node_Studio/formularios/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulário</title>
+</head>
+<body>
+    <form action="dados.php" method="post">
+        Nome: <input type="text" name="nome" id="nomeId"><br>
+        E-mail: <input type="email" name="email" id="emailId"><br>
+        <input type="submit" name="Enviar">
+    </form>
+</body>
+</html>
