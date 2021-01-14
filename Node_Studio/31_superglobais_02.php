b4064322d5c46696b340ef11f9bbbb16db1371d9
Under0Cover <60072961+Under0Cover@users.noreply.github.com>
2021-01-14 13:30:37 -0300

    Aula_31- Superglobais (02)

---
+++ Node_Studio/31_superglobais_02.php
@@ -0,0 +1,37 @@
+<?php
+
+// SUPERGLOBAIS
+/*
+    $GLOBALS    - USADA PARA ACESSAR VARIÁVEIS GLOBAIS DE QUALQUER LUGAR DO PHP
+    $_SERVER    - CONTÉM INFORMAÇÕES SOBRE CABEÇALHOS, CAMINHOS E LOCAIS DE SCRIPTS
+    $_REQUEST   -
+    $_POST      - UTILIZADA PARA COLETAR DADOS DE USUÁRIOS EM FORMULÁRIOS
+    $_GET       - UTILIZADA PARA COLETAR DADOS DE USUÁRIOS EM FORMULÁRIOS
+    $_FILES     -
+    $_ENV       -
+    $_COOKIE    -
+    $_SESSION   -
+*/
+
+// $GLOBALS
+$variavel_x = 10;
+$variavel_y = 15;
+
+function soma() {
+   echo $GLOBALS['variavel_x'] + $GLOBALS['variavel_y'];
+}
+
+soma();
+
+// $_SERVER
+echo $_SERVER['PHP_SELF'].'<br>';
+echo $_SERVER['SERVER_NAME'].'<br>';
+echo $_SERVER['SCRIPT_FILENAME'].'<br>';
+echo $_SERVER['DOCUMENT_ROOT'].'<br>';
+echo $_SERVER['DOCUMENT_ROOT'].'<br>';
+echo $_SERVER['SERVER_PORT'].'<br>';
+echo $_SERVER['REMOTE_ADDR'].'<br>';
+
+// 
+
+?>
