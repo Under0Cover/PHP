6dde45f2e175c10cabdaa35365eb90fa7e5bcb36
Under0Cover <60072961+Under0Cover@users.noreply.github.com>
2021-01-16 09:48:41 -0300

    Aula_37 - Sessões

---
+++ Node_Studio/sessoes/37-sessoes.php
@@ -0,0 +1,12 @@
+<?php
+
+session_star();
+
+$_SESSION['cor'] = 'Verder';
+$_SESSION['carro'] = 'Veloster';
+
+// echo $_SESSION['cor'].'<br>'.$_SESSION['carro'].'<br>'.session_id();
+
+
+
+?>
---
+++ Node_Studio/sessoes/logout.php
@@ -0,0 +1,11 @@
+<?php
+// Início de sessão
+session_start();
+
+// Para limpar a sessão
+session_unset();
+
+// Para destruir a sessão
+session_destroy();
+
+?>
