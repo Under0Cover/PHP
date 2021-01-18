d2f69dce5be0a2bf604b610f11471148163550ba
Under0Cover <60072961+Under0Cover@users.noreply.github.com>
2021-01-18 09:21:58 -0300

    Aula_41 - Include e Require

---
+++ Node_Studio/41_include_require/index.php
@@ -0,0 +1,11 @@
+<?php
+include 'header.php';
+
+echo 'Olá, pessoal!';
+
+include 'footer.php';
+
+// Require                              -> Caso dê erro, interrompe o script
+// Include                              -> Caso dê erro, não interrompe o script
+// include_once \/ require_once         -> Adiciona apenas uma vez
+?>
