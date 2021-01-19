17d6aa2cc1e3afc36fb6a8711228fdf2976f7ddf
Under0Cover <60072961+Under0Cover@users.noreply.github.com>
2021-01-18 21:16:36 -0300

    Aula_44 - Cookie

---
+++ Node_Studio/44_cookies.php
@@ -0,0 +1,7 @@
+<?php
+// Cookies
+setcookie('user', 'Oliver', time()+3600);
+setcookie('email', 'talis.oliveira@hotmail.com.br', time()+3600);
+setcookie('ultima_pesquisa', 'tenis adidas', time()+3600);
+
+?>
