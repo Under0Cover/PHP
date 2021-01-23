1e77d8a8deb4aa47d727714253643e0b6e1edde4
Under0Cover <60072961+Under0Cover@users.noreply.github.com>
2021-01-23 20:50:02 -0300

    aula_13 - namespaces

---
+++ Node_Studio_php_poo/modulos/produto.php
@@ -0,0 +1,13 @@
+<?php
+// NameSpaces
+
+// namespace com referência ao arquivo
+namespace modulos;
+
+class Produto {
+	public function mostrarDetalhes() {
+		echo 'Detalhes do Produto da Pasta Modulos';
+	}
+}
+
+?>
