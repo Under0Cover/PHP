c72a25bc4da57872d60b089745389b8908a313ec
Under0Cover <60072961+Under0Cover@users.noreply.github.com>
2021-02-05 08:58:13 -0300

    aula_03 - Tratamento de Erros

--- Digital_Innovation/aula_03/script.php
+++ Digital_Innovation/aula_03/script.php
@@ -8,51 +8,43 @@ $categorias[] = 'adulto';
 $nome = $_POST['nome'];
 $idade = $_POST['idade'];
 
-// Tratamento de erros e verificações de input
-	// Nome não pode ser vazio
-	if (empty($nome)) {
-		echo 'O nome não pode ser vazio';
-		return;
-	}
+// Nome não pode ser vazio
+if (empty($nome)) {
+	echo 'O nome não pode ser vazio';
+	return;
+}
 
-	// Nome não pode ser curto (menor que 3 caracteres)
-	if (strlen($nome) < 3) {
-		echo 'O nome deve conter mais de 3 caracteres!';
-		return;
-	}
+// Nome não pode ser curto (menor que 3 caracteres)
+if (strlen($nome) < 3) {
+	echo 'O nome deve conter mais de 3 caracteres!';
+	return;
+}
 
-	// Nome não pode ser longo (maior que 20 caracteres)
-	if (strlen($nome) > 20) {
-		echo 'O nome é muito extenso!';
-		return;
-	}
-	
-	// O php trata nativamente o recebimento de inputs como string, aqui vamos verificar se essa string é númerica
-	// O tratamento do erro é caso essa string NÃO SEJA NÚMERICA
-	if (!is_numeric($idade)) {
-		echo 'Informe um número para a sua idade!'
-		return;
-	}
+// Nome não pode ser longo (maior que 20 caracteres)
+if (strlen($nome) > 20) {
+	echo 'O nome é muito extenso!';
+	return;
+}
 
 // Verificação de idades para separação das categorias
-		// Infantil
-	if ($idade < 13) {
-		for ($indice = 0; $indice <= count($categorias); $indice++) {
-			if ($categorias[$indice] == 'infantil')
-				echo 'O nadador '.$nome. ' compete na categoria ' .$categorias[$indice];
-		}
-		// Adolescente
-	} else if ($idade < 19) {
-		for ($indice = 0; $indice <= count($categorias); $indice++) {
-			if ($categorias[$indice] == 'adolescente')
-				echo 'O nadador '.$nome. ' compete na categoria ' .$categorias[$indice];
-		}
-		// Adulto
-	} else {
-		for ($indice = 0; $indice <= count($categorias); $indice++) {
-			if ($categorias[$indice] == 'adulto')
-				echo 'O nadador '.$nome. ' comepte na categoria' .$categorias[$indice];
-		}
+	// Infantil
+if ($idade < 13) {
+	for ($indice = 0; $indice <= count($categorias); $indice++) {
+		if ($categorias[$indice] == 'infantil')
+			echo 'O nadador '.$nome. ' compete na categoria ' .$categorias[$indice];
 	}
+	// Adolescente
+} else if ($idade < 19) {
+	for ($indice = 0; $indice <= count($categorias); $indice++) {
+		if ($categorias[$indice] == 'adolescente')
+			echo 'O nadador '.$nome. ' compete na categoria ' .$categorias[$indice];
+	}
+	// Adulto
+} else {
+	for ($indice = 0; $indice <= count($categorias); $indice++) {
+		if ($categorias[$indice] == 'adulto')
+			echo 'O nadador '.$nome. ' comepte na categoria' .$categorias[$indice];
+	}
+}
 
 ?>
