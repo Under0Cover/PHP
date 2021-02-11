1869aad619499c097afd5b187aa3813e37a24449
Under0Cover <60072961+Under0Cover@users.noreply.github.com>
2021-02-11 18:02:21 -0300

    aula_08 - Introdução à funções

--- Digital_Innovation/aula_08/script.php
+++ Digital_Innovation/aula_08/script.php
@@ -16,30 +16,6 @@ $categorias[] = 'adulto';
 $nome = $_POST['nome'];
 $idade = $_POST['idade'];
 
-// Tratamento de erros e verificações de input
-	// Nome não pode ser vazio
-	if (empty($nome)) {
-		$_SESSION['SessionErro'] = 'O nome não pode ser vazio!';
-		header('Location: index.php');
-		return;
-	} else if (strlen($nome) < 3) {
-		// Nome não pode ser curto (menor que 3 caracteres)
-		$_SESSION['SessionErro'] = 'O nome não pode ser menor que 3 caracteres!';
-		header('Location: index.php');
-		return;
-	} else if (strlen($nome) > 20) {
-		// Nome não pode ser longo (maior que 20 caracteres)
-		$_SESSION['SessionErro'] = 'O nome é muito extenso!';
-		header('Location: index.php');
-		return;
-	} else if (!is_numeric($idade)) {
-		// O php trata nativamente o recebimento de inputs como string, aqui vamos verificar se essa string é númerica
-		// O tratamento do erro é caso essa string NÃO SEJA NÚMERICA
-		$_SESSION['SessionErro'] = 'Informe um número para a idade!';
-		header('Location: index.php');
-		return;
-	}
-
 // Verificação de idades para separação das categorias
 		// Infantil
 	if ($idade < 13) {
