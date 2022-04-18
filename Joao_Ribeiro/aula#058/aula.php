d4b4d2cd98b678fd01996a780aae4e4d43234efa
Tales Oliver <talis.oliveira@hotmail.com.br>
2022-04-18 16:57:38 -0300

    aula 58

---
+++ Joao_Ribeiro/aula#058/aula.php
@@ -0,0 +1,14 @@
+<?php
+    /*
+        GET
+        Passando valores entre páginas web
+
+        Através da query string podemos passar valores, como visto na última aula.
+        No entanto, uma vez que é muito fácil adulterar a query string, não podemos correr o risco de tentar capturar uma variável que não existe.
+
+        E se quisermos verificar se determinada variável existe na query string?
+
+
+        Vejamos:
+    */
+?>
