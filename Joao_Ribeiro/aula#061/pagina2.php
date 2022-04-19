7e3e19cdaa42ac53fd644d37286a57e464b5e73d
Tales Oliver <talis.oliveira@hotmail.com.br>
2022-04-19 09:37:50 -0300

    aula 61

---
+++ Joao_Ribeiro/aula#061/pagina2.php
@@ -0,0 +1,25 @@
+<?php
+    if(!isset($_GET['nome'])){
+        // exit();
+        // die();
+        header('Location: pagina1.php');
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página 2</title>
+</head>
+<body>
+    <?php 
+        $nome = htmlspecialchars($_GET['nome']);
+    ?>
+    <h3>Página 2</h3>
+    <p><?= $nome ?></p>
+    <a href="pagina1.php">Voltar para a página 1</a>.
+</body>
+</html>
