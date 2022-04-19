13627f16c5c0df4e40c292471c7ee43067f275ad
Tales Oliver <talis.oliveira@hotmail.com.br>
2022-04-19 08:53:51 -0300

    aula 60

---
+++ Joao_Ribeiro/aula#060/aula.php
@@ -0,0 +1,9 @@
+<?php
+    /*
+        GET
+        Passando valores entre páginas web
+
+        Todo o cuidado é pouco quando passamos variáveis por query strings.
+        Vejamos uma situação que pode comprometer a nossa página e como solucionar o problema.
+    */
+?>
