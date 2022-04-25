98f73e3db55837c7def67ef088ff3b145056ef39
Tales Oliver <talis.oliveira@hotmail.com.br>
2022-04-25 20:13:26 -0300

    aula 74

--- Joao_Ribeiro/aula#072/submit/tratar_correto.php
+++ Joao_Ribeiro/aula#072/submit/tratar_correto.php
@@ -8,8 +8,17 @@
     // Destino final é "C:/temp"
     $pasta_final = 'C:'.DIRECTORY_SEPARATOR.'temp'.DIRECTORY_SEPARATOR;
 
+    $ficheiros_aceitos = [
+        'image/png',
+        'image/jpeg'
+    ];
+
     // Tratamento dos ficheiros
     foreach($_FILES as $file){
+        // verifica se a imagem é do tipo esperado
+        if(in_array($file['type'], $ficheiros_aceitos)){
+            continue;
+        }
 
         // mover o ficheiro para o destino final
         move_uploaded_file($file['tmp_name'], $pasta_final.$file['name']);
