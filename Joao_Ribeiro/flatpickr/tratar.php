b85f94f4f1f40f3e07c559dfb8d01c46a4430779
Tales Oliver <talis.oliveira@hotmail.com.br>
2022-04-25 10:11:55 -0300

    flatpick - parte 2

--- Joao_Ribeiro/flatpickr/tratar.php
+++ Joao_Ribeiro/flatpickr/tratar.php
@@ -1,4 +1,15 @@
 <?php
-    echo '<pre>';
-    print_r($_POST);
+    $valores = $_POST['data_nascimento'];
+    $periodo = $_POST['periodo_dias'];
+
+    $tratamento_periodo = explode(" to ", $periodo);
+    $tratamento = explode(" ", $valores);
+    
+    $data_inicial = $tratamento_periodo[0];
+    $data_final = $tratamento_periodo[1];
+    $data = $tratamento[0];
+    $hora = $tratamento[1];
+
+    echo "Minha data de nascimento é $data.<br>E a minha hora de nascimento é $hora.<br>";
+    echo "As suas férias começam em: $data_inicial e terminam em: $data_final.";
 ?>
