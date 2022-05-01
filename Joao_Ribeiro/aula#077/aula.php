92cae7e06b4434df9b35917e9d394b21af82c8b7
Tales Oliver <talis.oliveira@hotmail.com.br>
2022-05-01 15:19:17 -0300

    aula 77

--- Joao_Ribeiro/aula#077/aula.php
+++ Joao_Ribeiro/aula#077/aula.php
@@ -1,13 +1,10 @@
 <?php
     
-    class Sql extends PDO {
-        private $conn;
+    $gestor = new PDO("sqlsrv:Database=loja; server=OLIVER\SQLEXPRESS; ConnectionPooling=0", "teknisa", "teknisa");
+    $dados = $gestor->query("SELECT * FROM CLIENTES");
+    $clientes = $dados->fetchAll(PDO::FETCH_ASSOC);
 
-        public function __construct() {
-            $this->conn = new PDO("sqlsrv:Database=dbphp7; server=OLIVER\SQLEXPRESS; ConnectionPooling=0", "teknisa", "teknisa");
-        }
-    }
-
-
-
+    echo '<pre>';
+    print_r($clientes);
+    
 ?>
