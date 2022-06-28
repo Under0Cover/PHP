dbcf569ec62a372cba131007c45febccc3523f24
Tales Oliver <talis.oliveira@hotmail.com.br>
2022-06-28 20:52:33 -0300

    UPDATE TESTADO

---
+++ Joao_Ribeiro/aula#087/configs/config.php
@@ -0,0 +1,20 @@
+<?php
+    
+    // Definição com o Banco de Dados 01
+    define('CONEXAO_ANTIGA', [
+        'localBancoDados' => 'OLIVER\SQLEXPRESS',
+        'nomeBancoDados' => 'loja',
+        'usuarioBancoDados' => 'teknisa',
+        'senhaBancoDados' => 'teknisa',
+    ]);
+
+    
+    // Definição com o Banco de Dados 01
+    define('CONEXAO_NOVA', [
+        'localBancoDados' => 'localhost',
+        'nomeBancoDados' => '',
+        'usuarioBancoDados' => '',
+        'senhaBancoDados' => '',
+    ]);
+
+    
---
+++ Joao_Ribeiro/aula#087/index.php
@@ -0,0 +1,27 @@
+<?php
+
+    /*
+        Antes de assistir a aula...
+            A comunicação com o banco de dados na Classe PDOFacil está estabelecida.
+            O Insert também está configurado.
+
+            As outras instruções SQL estão configuradas e nessa aula será testadas as funcionalidades.
+            Erros e problemas serão corrigidos
+    */
+    use PDOFacil\PDOFacil;
+
+    require_once('libs'.DIRECTORY_SEPARATOR.'PDOFacil.php');
+    require_once('configs'.DIRECTORY_SEPARATOR.'config.php');
+
+    // A forma de trazer a conexão com o banco de dados, quando utiliza mais de um banco
+    $gestor = new PDOFacil(CONEXAO_ANTIGA);
+
+    // UPDATE
+    $parametros = [
+        ':nome' => 'CRISTINA',
+        ':id' => '6'
+    ];
+
+    $gestor->update("UPDATE CLIENTES SET NOME = :nome WHERE ID_CLIENTE = :id", $parametros);
+
+?>
