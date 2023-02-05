7f70556be82943b1ec3ce27cdc653a1d9a62eb8f
Tales Oliver <talis.oliveira@hotmail.com.br>
2023-02-05 20:31:25 -0300

    aula 90 - conhecendo slim framework

--- HCode_Udemy/WebSite/index.php
+++ HCode_Udemy/WebSite/index.php
@@ -1,5 +1,21 @@
 <?php
 
-    phpinfo();
+    require_once("vendor/autoload.php");
+
+
+    use Slim\Slim as Slim;
+
+    $app = new Slim();
+    $app->get('/hello/:name', function ($name){
+        echo "Hello, ". $name;
+    });
+
+    $app->get('/', function(){
+        echo json_encode(array(
+            'date'=>date("Y-m-d H:i")
+        ));
+    });
+
+    $app->run();
 
 ?>
